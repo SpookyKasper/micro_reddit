0d0751e375c26d0fb74938fd38aa558072a19084
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-02-20 13:05:25 +0100

    Implement a micro reddit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 6 }
